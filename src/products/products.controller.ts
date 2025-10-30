b999bac9e258513bbdac341c5c90969c5eb59db2
VolodyaHovhannisyan <volodyahovhannisyan000@gmail.com>
2025-10-30 22:15:30 +0400

    Day 4 - Full CRUD with Prisma + Nest.js

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, ParseIntPipe, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 
@@ -6,13 +6,28 @@ import { CreateProductDto } from './dto/create-product.dto';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Get()
-  findAll() {
-    return this.productsService.findAll();
+  @Post()
+  create(@Body() dto: CreateProductDto) {
+    return this.productsService.create(dto);
   }
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Get()
+  findAll(@Query('skip') skip?: number,@Query('take') take?: number,) {
+    return this.productsService.findAll(skip, take);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: Partial<CreateProductDto>) {
+    return this.productsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
   }
 }
