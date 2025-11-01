218d5b749f7384ba9a508c6bd0ca08c24aa5bc3f
VolodyaHovhannisyan <volodyahovhannisyan000@gmail.com>
2025-11-01 14:20:18 +0400

    Day 6 - React + Vite + TypeScript setup and connected to API

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
-  .setTitle('Products API')
-  .setDescription('CRUD API built with Nest.js + Prisma')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Products API')
+    .setDescription('CRUD API built with Nest.js + Prisma')
+    .setVersion('1.0')
+    .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
